029aa4db1ba310234388f3d5e7dd10d6af431643
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2024-05-18 22:01:49 +0300

    Added a simple drawing.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use rust_tic_tac_toe::{event, game::Game};
-use sdl2::event::Event;
+use sdl2::{event::Event, pixels, rect::Rect};
 
 fn main() -> Result<(), String> {
     let sdl = sdl2::init()?;
     let video = sdl.video()?;
-    let _window = video
+    let window = video
         .window("Tic-Tac-Toe", 800, 600)
         .build()
         .map_err(|e| e.to_string())?;
+    let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
     let mut event_pump = sdl.event_pump()?;
 
     let mut game = Game::new();
@@ -27,6 +28,12 @@ fn main() -> Result<(), String> {
         }
         game.push_event(event::Event::Update);
         game.tick();
+
+        canvas.set_draw_color(pixels::Color::RGB(0, 0, 0));
+        canvas.clear();
+        canvas.set_draw_color(pixels::Color::RGB(255, 0, 0));
+        canvas.draw_rect(Rect::new(100, 100, 100, 100))?;
+        canvas.present()
     }
     Ok(())
 }
